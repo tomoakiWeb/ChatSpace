777ebfe448371d60d62418a18f87ecc7dd52934a
tomoakimiura <tomomiura0026@gmail.com>
2019-02-25 16:33:51 +0900

    Edit.factoryBot

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -1,8 +1,8 @@
-FactoryBot.define do
-  factory :message do
-    content Faker::Lorem.sentence
-    # image File.open("#{Rails.root}/public/images/no_image.jpg")
-    user
-    group
-  end
-end
+# FactoryBot.define do
+#   factory :message do
+#     content Faker::Lorem.sentence
+#     # image File.open("#{Rails.root}/public/images/no_image.jpg")
+#     user
+#     group
+#   end
+# end

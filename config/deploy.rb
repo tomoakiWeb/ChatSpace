0e070a28f9c3eee9d5c8214859a7e6f7430a156f
tomoakimiura <tomomiura0026@gmail.com>
2019-02-25 17:55:09 +0900

    自動デプロイ

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
-set :ssh_options, auth_methods: ['awskey'],
+set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/awskey.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
